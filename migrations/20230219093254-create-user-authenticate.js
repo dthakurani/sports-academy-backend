be38674c71e759b1e9ced50049c71ee8075b6e78
divyanshi thakurani <divyanshi.thakurani@gmail.com>
2023-02-19 15:42:45 +0530

    create user authenticate model

--- migrations/20230219093254-create-user-authenticate.js
+++ migrations/20230219093254-create-user-authenticate.js
@@ -1,36 +1,26 @@
-"use strict";
+'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("user", {
+    await queryInterface.createTable("user_authenticate", {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.literal('uuid_generate_v4()')
       },
-      name: {
+      user_id: {
+        type: Sequelize.UUID,
+        references: {
+          model: "user",
+          key: "id",
+        },
+      },
+      refreshTokenId: {
         type: Sequelize.STRING,
       },
-      email: {
+      accessTokenId: {
         type: Sequelize.STRING,
-      },
-      password: {
-        type: Sequelize.STRING,
-      },
-      role: {
-        type: Sequelize.ENUM,
-        values: ["admin", "user"],
-        defaultValue: "user",
-      },
-      resetPasswordToken: {
-        type: Sequelize.STRING,
-      },
-      resetPasswordExpires: {
-        type: Sequelize.BIGINT,
-      },
-      resetPasswordTokenUse: {
-        type: Sequelize.BOOLEAN,
       },
       created_at: {
         allowNull: false,
@@ -42,14 +32,9 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.NOW,
       },
-      deleted_at: {
-        allowNull: true,
-        type: Sequelize.DATE,
-        defaultValue: null,
-      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("user");
-  },
+    await queryInterface.dropTable("user_authenticate");
+  }
 };
