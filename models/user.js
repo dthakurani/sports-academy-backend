f8c761529962e7358a36f6566cc923b9a95c3bff
divyanshigkmit <divyanshi@gkmit.co>
2023-02-19 14:46:17 +0530

    Feat: Create user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model, Sequelize } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      resetPasswordToken: {
+        type: Sequelize.STRING,
+      },
+      resetPasswordExpires: {
+        type: Sequelize.BIGINT,
+      },
+      resetPasswordTokenUse: {
+        type: Sequelize.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      paranoid: true,
+      modelName: "User",
+      tableName: "user",
+    }
+  );
+  return User;
+};
