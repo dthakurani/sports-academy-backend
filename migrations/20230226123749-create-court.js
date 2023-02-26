4128e84022630026a14aaec17b24f9042cbac1bc
divyanshi thakurani <divyanshi.thakurani@gmail.com>
2023-02-26 18:22:24 +0530

    added model for court

---
+++ migrations/20230226123749-create-court.js
@@ -0,0 +1,27 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Courts', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()')
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Courts');
+  }
+};
