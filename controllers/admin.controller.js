c26155fa2b4a6db0a0f0474deea84884072229d3
Dharmishtha Ajmera <dharmishthaajmera@gmail.com>
2023-03-18 19:06:19 +0530

    added add court details changes to add court api

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,15 +1,17 @@
+const sequelize = require('sequelize');
 const models = require('../models');
-const { customException, commonErrorHandler } = require('../helper/errorHandler');
+const { customException } = require('../helper/errorHandler');
 
 const addCourt = async (req, res, next) => {
+  const t = await sequelize.transaction();
   try {
-    const { name } = req.body;
+    const { name, bookingType, capacity, count } = req.body;
 
     const existingCourt = await models.Court.findOne({
       where: {
         name
       }
-    });
+    }, { transaction: t });
 
     if (existingCourt) {
       throw customException('Court already exists', 409);
@@ -17,17 +19,27 @@ const addCourt = async (req, res, next) => {
 
     const newCourt = await models.Court.create({
       name
-    });
+    }, { transaction: t });
+
+    const newCourtDetails = await models.CourtDetail.create({
+        courtId: newCourt.dataValues.id,
+        bookingType,
+        capacity,
+        count
+    }, { transaction: t });
 
     req.statusCode = 201;
     req.data = {
-      newCourt: newCourt.dataValues.name
+      newCourt: newCourt.dataValues.name,
+      bookingType: newCourtDetails.dataValues.bookingType,
+      capacity: newCourtDetails.dataValues.capacity,
+      count: newCourtDetails.dataValues.count
     };
+    await t.commit();
     next();
   } catch (error) {
+    await t.rollback();
     console.log('addCourt error:', error);
-    const statusCode = error.statusCode || 500;
-    commonErrorHandler(req, res, error.message, statusCode, error);
   }
 };
 
--- validators/admin.validator.js
+++ validators/admin.validator.js
@@ -7,7 +7,10 @@ const { validator } = require('../helper/validator');
 const addCourt = async (req, res, next) => {
   const schema = yup.object({
     body: yup.object({
-      name: yup.string().typeError(responseMessages.COURT_NAME_MUST_BE_STRING).required(responseMessages.COURT_NAME_IS_REQUIRED)
+      name: yup.string().typeError(responseMessages.COURT_NAME_MUST_BE_STRING).required(responseMessages.COURT_NAME_IS_REQUIRED),
+      bookingType: yup.string().oneOf(['single', 'multiple', ''], responseMessages.INVALID_VALUE_FOR_BOOKING_TYPE),
+      capacity: yup.number().integer().positive().nullable(true),
+      count: yup.number().integer().positive().nullable(true)
     })
   });
   validator(req, res, schema, next);
