b1cf0e1b32939a626fd86017fed9df60e600c475
Dharmishtha Ajmera <dharmishthaajmera@gmail.com>
2023-03-26 13:03:52 +0530

    changes in api for falied cases

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -8,85 +8,84 @@ const { STATUS } = require('../constants');
 const addBooking = async (req, res, next) => {
   const t = await sequelize.transaction();
   try {
-    const { userId, courtId, date, startTime, endTime } = req.body;
+        const { courtId, date, startTime, endTime } = req.body;
+        const userId = req.user;
 
-    const existingUser = await model.User.findOne({
-      id: userId
-    });
-    if (!existingUser) {
-      customException('User not exists', 404);
+        const existingUser = await model.User.findOne({
+            id: userId
+        }
+        );
+        if (!existingUser) {
+            customException('User not exists', 404);
+        }
+
+        const existingCourt = await model.Court.findOne({
+            id: courtId
+        }
+        );
+        if (!existingCourt) {
+            customException('Court not exists', 404);
+        }
+
+        const bookingExists = await model.Booking.findAll({
+            where: {
+                startTime: {
+                    [Op.eq]: startTime
+                },
+                endTime: {
+                    [Op.eq]: endTime
+                },
+                courtId,
+                date
+            }
+        }
+        );
+
+        for (const booking of bookingExists) {
+            if (booking.dataValues.userId === userId) {
+                customException('booking exists for respective court and time.', 409);
+                break;
+            }
+        }
+
+        const courtDetails = await model.CourtDetail.findOne({
+            courtId
+            });
+
+        if (bookingExists.length < (courtDetails.dataValues.count * courtDetails.dataValues.capacity)) {
+            await model.Booking.create(
+                {
+                courtId,
+                userId,
+                date,
+                startTime,
+                endTime,
+                status: STATUS.PENDING
+                },
+                { transaction: t }
+            );
+        } else {
+        customException('Court is not available for prefered time.', 409);
+        }
+
+        req.data = {
+        userId,
+        courtId,
+        date,
+        startTime,
+        endTime,
+        status: 'pending'
+        };
+
+        await t.commit();
+        req.statusCode = 201;
+        next();
+    } catch (error) {
+        await t.rollback();
+        console.log('create booking error:', error);
+        const statusCode = error.statusCode || 500;
+        commonErrorHandler(req, res, error.message, statusCode, error);
     }
-
-    const existingCourt = await model.Court.findOne({
-      id: courtId
-    });
-    if (!existingCourt) {
-      customException('Court not exists', 404);
-    }
-
-    const bookingExists = await model.Booking.findAll({
-      where: {
-        startTime: {
-          [Op.eq]: startTime
-        },
-        endTime: {
-          [Op.eq]: endTime
-        },
-        courtId
-      }
-    });
-    if (bookingExists) {
-      for (const booking of bookingExists) {
-        console.log(booking.dataValues);
-      }
-    }
-
-    const courtDetails = await model.CourtDetail.findOne({
-      courtId
-    });
-    if (!courtDetails.dataValues.capacity) {
-      await model.Booking.create(
-        {
-          courtId,
-          userId,
-          date,
-          startTime,
-          endTime,
-          status: STATUS.PENDING
-        },
-        { transaction: t }
-      );
-
-      await model.CourtDetail.update(
-        {
-          count: courtDetails.dataValues.count++
-        },
-        {
-          where: { courtId }
-        },
-        { transaction: t }
-      );
-    } else {
-      customException('Court is not available for prefered time.', 409);
-    }
-    req.data = {
-      userId,
-      courtId,
-      date,
-      startTime,
-      endTime,
-      status: 'pending'
-    };
-
-    await t.commit();
-    req.statusCode = 201;
-    next();
-  } catch (error) {
-    await t.rollback();
-    console.log('create booking error:', error);
-    const statusCode = error.statusCode || 500;
-    commonErrorHandler(req, res, error.message, statusCode, error);
-  }
 };
 
 module.exports = {
--- validators/booking.validator.js
+++ validators/booking.validator.js
@@ -1,5 +1,4 @@
 const yup = require('yup');
-require('yup-password')(yup);
 
 const { responseMessages } = require('../constants');
 const { validator } = require('../helper/validator');
@@ -9,10 +8,9 @@ const addBooking = async (req, res, next) => {
     body: yup.object({
       userId: yup.string().uuid(responseMessages.ID_VALIDATION).required(responseMessages.ID_REQUIRED),
       courtId: yup.string().uuid(responseMessages.ID_VALIDATION).required(responseMessages.ID_REQUIRED),
-      date: yup.date(responseMessages.DATE_TIME_VALIDATION),
+      date: yup.date().typeError(responseMessages.DATE_TIME_VALIDATION),
       startTime: yup.string(responseMessages.DATE_TIME_VALIDATION),
       endTime: yup.string("end date can't be before start time"),
-
       status: yup.string().oneOf(['successful', 'cancel', 'reject', 'pending'], responseMessages.INVALID_VALUE_FOR_STATUS)
     })
   });
